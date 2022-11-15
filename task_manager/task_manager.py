543f232be026d6c3ea54ca516e708dc7c0c73ae4
azerpas <xskylix@hotmail.fr>
2022-11-15 11:25:28 +0100

    feat: parse_command quit

--- task_manager/task_manager.py
+++ task_manager/task_manager.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from task_manager.task import Task
 
 class TaskManager:
@@ -28,14 +29,21 @@ class TaskManager:
         else:
             raise ValueError("Unknown task id")
 
-    def parse_command(self, command) -> None:
+    def parse_command(self, command) -> Dict[str, str]:
         if command.startswith("+ "):
             self.add_task(command[2:], "to do")
+            return {"type": "add"}
         elif command.startswith("- "):
             self.remove_task(command[2:])
+            return {"type": "remove"}
         elif command.startswith("x "):
             self.change_task_status(command[2:], "done")
+            return {"type": "status"}
         elif command.startswith("o "):
             self.change_task_status(command[2:], "to do")
+            return {"type": "status"}
+        elif command == "q":
+            return {"type": "quit"}
         else:
             raise ValueError("Unknown command")
+    
