fd8a12c9d15a2e06bc878c3d7d7b10474165f6af
azerpas <xskylix@hotmail.fr>
2022-11-15 10:35:46 +0100

    refactor: Task Manager add_task method with counter

--- task_manager/task_manager.py
+++ task_manager/task_manager.py
@@ -4,6 +4,9 @@ from task_manager.task import Task
 class TaskManager:
     def __init__(self) -> None:
         self.tasks = []
+        self.counter = 1
 
-    def add_task(self, task: Task) -> None:
+    def add_task(self, description, status) -> None:
+        task = Task(self.counter, description, status)
         self.tasks.append(task)
+        self.counter += 1
