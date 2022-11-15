eb566ec873dde5c09836b286b95fb42216e2f7d8
Aimen-Allah <aimenftaiti@gmail.com>
2022-11-15 10:28:07 +0100

    feat: added add_task method

--- task_manager/task_manager.py
+++ task_manager/task_manager.py
@@ -1,3 +1,9 @@
+from task_manager.task import Task
+
+
 class TaskManager:
     def __init__(self) -> None:
         self.tasks = []
+
+    def add_task(self, task: Task) -> None:
+        self.tasks.append(task)
