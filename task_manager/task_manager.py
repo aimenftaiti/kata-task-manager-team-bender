ceb5c177b193d4d0d799b00a8ca20a6901dc1f96
azerpas <xskylix@hotmail.fr>
2022-11-15 10:51:02 +0100

    feat: handle remove task

--- task_manager/task_manager.py
+++ task_manager/task_manager.py
@@ -10,8 +10,19 @@ class TaskManager:
         self.tasks.append(task)
         self.counter += 1
 
+    def remove_task(self, id) -> None:
+        id = int(id)
+        for task in self.tasks:
+            if task.id == id:
+                self.tasks.remove(task)
+                break
+        else:
+            raise ValueError("Unknown task id")
+
     def parse_command(self, command) -> None:
         if command.startswith("+ "):
             self.add_task(command[2:], "to do")
+        elif command.startswith("- "):
+            self.remove_task(command[2:])
         else:
             raise ValueError("Unknown command")
