5928295a1819ad0497bca8884574008854892a3a
Aimen-Allah <aimenftaiti@gmail.com>
2022-11-15 10:26:34 +0100

    test: test_task_manager_add_task added

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -3,6 +3,14 @@ import pytest
 from task_manager.task import Task
 from task_manager.task_manager import TaskManager
 
+
 def test_task_manager_init():
     task_manager = TaskManager()
     assert task_manager.tasks == []
+
+
+def test_task_manager_add_task():
+    task_manager = TaskManager()
+    task = Task(1, "description", "to do")
+    task_manager.add_task(task)
+    assert task_manager.tasks == [task]
